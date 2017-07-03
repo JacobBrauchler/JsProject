f2b737ea6d8dc983b011e96717b810d8a0c2706f
kenzan admin <kenzanadmin@kenzans-MacBook-Pro.local>
2017-07-03 15:40:34 -0600

    initializing project

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,8 @@
+// This file isn't transpiled, so must use CommonJs and ES5
+
+// Register bable to transpile before our tests run.
+
+require('babel-register')();
+
+// Disable webpack features that Mocha doesn;t understand
+require.extensions['css'] = function() {};
